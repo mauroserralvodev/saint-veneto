4487c9634a3bd268c36d3fc0247c4457a66aabb7
moozart-tech <mauroserralvo@gmail.com>
2025-02-16 17:28:36 +0100

    Footer text change

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
           <a className="flex min-w-max title-font items-center md:justify-start justify-center text-gray-900">
             <img src='/logo.png' className='w-96 px-5'/>
           </a>
-          <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-10 px-5">©2025 SAINT VENETO —
+          <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-10 px-3">©2025 SAINT VENETO —
             <a href="https://instagram.com/mauroserralvo" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">CREATIVE DIRECTOR @mauroserralvo </a>
             — All rights on the content are reserved, saintveneto is a registered trademark. Website created by Brinpage Software all rights and functionalities of the software are property of<a href="https://brinpage.com/" className="text-gray-600 ml-1 hover:underline" rel="noopener noreferrer" target="_blank">Brinpage Software</a>.
           </p>
